b2f270f99b00ed0fec85f472b69e791a82b214e9
VladStarostenko <vlad.starostenko@data-lake.co>
2022-10-11 15:13:06 +0200

    Fix ENV loading

--- src/dapp-config.ts
+++ src/dapp-config.ts
@@ -4,7 +4,11 @@ export const dappConfig = {
     autoConnect: false,
     readOnlyChainId: Mainnet.chainId,
     readOnlyUrls: {
-        [Mainnet.chainId]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_ETHEREUM_KEY}`,
-        [Goerli.chainId]: `https://eth-goerli.g.alchemy.com/v2/${process.env.REACT_APP_GOERLI_ETHEREUM_KEY}`,
+        [Mainnet.chainId]: `https://eth-mainnet.g.alchemy.com/v2/${
+            import.meta.env.VITE_ALCHEMY_ETHEREUM_KEY
+        }`,
+        [Goerli.chainId]: `https://eth-goerli.g.alchemy.com/v2/${
+            import.meta.env.VITE_ALCHEMY_GOERLI_KEY
+        }`,
     },
 };
