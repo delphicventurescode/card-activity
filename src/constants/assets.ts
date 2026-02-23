3afb28b40a0d13c6b0e25bb0be1be8c38950639e
Sam Qwykr <qwykrcooking@gmail.com>
2026-02-23 19:15:07 +0530

    Fix: Restore missing constants and resolve build errors

---
+++ src/constants/assets.ts
@@ -0,0 +1,41 @@
+import { IAsset } from '../interfaces/asset.interface';
+
+export const ASSET_USDT: IAsset = {
+    name: 'Tether USD',
+    symbol: 'USDT',
+    decimals: 6,
+};
+
+export const ASSET_LAKE: IAsset = {
+    name: 'Data Lake Token',
+    symbol: 'LAKE',
+    decimals: 18,
+    image: 'https://data-lake.co/wp-content/uploads/2022/07/DL-Logo-Mark-Black.png',
+};
+
+export const ASSET_ETH: IAsset = {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+};
+
+export const ASSET_DAI: IAsset = {
+    name: 'DAI',
+    symbol: 'DAI',
+    decimals: 18,
+};
+
+export const ASSET_USDC: IAsset = {
+    name: 'Tether USDC',
+    symbol: 'USDC',
+    decimals: 6,
+};
+
+export const ASSET_WETH: IAsset = {
+    name: 'Wrapped Ether',
+    symbol: 'WETH',
+    decimals: 18,
+};
+
+// Update this array to include both new assets
+export const ASSETS: IAsset[] = [ASSET_LAKE, ASSET_ETH, ASSET_DAI, ASSET_USDC, ASSET_USDT, ASSET_WETH];
